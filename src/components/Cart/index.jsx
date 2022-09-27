951c2c0df99d8b1940824e2cdf08d1c3930bccff
Lauta Hern <lautaa.94@gmail.com>
2022-09-27 14:00:20 -0300

    order with firebase

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
 import ItemCart from '../ItemCart';
 
 const Cart = () => {
-  const { cart, totalPrice, deleteAll } = useCartContext();
+  const { cart, totalPrice, deleteAll, totalProducts } = useCartContext();
+
+  const order = {
+      buyer: {
+        name: 'Lalo Landa',
+        email: 'lalo@coder.com.ar',
+        phone: '255345',
+        address: '742 Evergreen Terrace'
+      },
+      items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity})), 
+      total: totalPrice(),
+  }
+
+  const handleClick = ()=> {
+    const db = getFirestore();
+    const orderCollection = collection(db, 'orders');
+    addDoc(orderCollection, order)
+      .then(({ id }) => (
+        alert(`
+        Id de compra Nº ${ id }
+        Nombre: ${order.buyer.name}
+        Email: ${order.buyer.email}
+        Teléfono: ${order.buyer.phone}
+        Dirección: ${order.buyer.address}
+        Items: ${totalProducts()}
+        Total: $${order.total}
+        ¡Muchas gracias por confiar en nosotros!`)))
+  }
 
   if (cart.length === 0) {
     return(
@@ -24,7 +52,8 @@ const Cart = () => {
         <p className='h3'>
         Total: $ {totalPrice()}
       </p>
-      <Link className='btn btn-dark text-white rounded-pill' to='/'>Seguir comprando</Link><br></br><br></br>
+      <button className='btn btn-dark border text-white rounded-pill' onClick={handleClick}>Comprar</button>
+      <Link className='btn btn-dark text-white rounded-pill' to='/'>Seguir comprando</Link>
       <button className='btn btn-dark text-white rounded-pill' onClick={deleteAll}>Vaciar carrito</button>
       </div>
       
